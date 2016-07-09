b218e08743c078d86fd43b70d8b0248f00a48801
Beto Muniz <contato@betomuniz.com>
2016-07-09 10:35:48 -0300

    Remove lx-valid to avoid redundancies and complexities

--- hapi-rethinkdb-api/models/tasks.js
+++ hapi-rethinkdb-api/models/tasks.js
@@ -1,31 +1,10 @@
 import r from 'rethinkdb';
-import val from 'lx-valid';
 import config from '../config.js';
 import db from '../helpers/db';
 
 export default class Tasks {
   constructor() {
     this.tableName = 'tasks';
-
-    this.fields = {
-      properties: {
-        name: {
-          type: 'string',
-          trim: true,
-          required: true
-        },
-        done: {
-          type: 'boolean'
-        }
-      }
-    };
-  }
-
-  validate(data, isUpdate = false) {
-    const { schema } = config.model;
-    schema.isUpdate = isUpdate;
-    const validate = val.getValidationFunction();
-    return validate(data, this.fields, schema);
   }
 
   list(done) {
@@ -53,44 +32,34 @@ export default class Tasks {
   }
 
   insert(task, done) {
-    const validation = this.validate(task, false);
-    if (validation.valid) {
-      db.connect().then((dbConn) => {
-        r.table(this.tableName)
-          .insert(task)
-          .run(dbConn)
-          .then(result => {
-            r.table(this.tableName)
-              .get(result.generated_keys[0])
-              .run(dbConn)
-              .then(newTask => done(null, newTask))
-              .error(err => done(err))
-            ;
-          })
-          .error(err => done(err));
-      });
-    } else {
-      done(validation.errors);
-    }
+    db.connect().then((dbConn) => {
+      r.table(this.tableName)
+        .insert(task)
+        .run(dbConn)
+        .then(result => {
+          r.table(this.tableName)
+            .get(result.generated_keys[0])
+            .run(dbConn)
+            .then(newTask => done(null, newTask))
+            .error(err => done(err))
+          ;
+        })
+        .error(err => done(err));
+    });
   }
 
   update(taskId, task, done) {
-    const validation = this.validate(task, true);
-    if (validation.valid) {
-      db.connect().then((dbConn) => {
-        r.table(this.tableName)
-          .get(taskId)
-          .update(task, config.model.update)
-          .run(dbConn)
-          .then(result => {
-            const { new_val } = result.changes[0] || {};
-            done(null, new_val);
-          })
-          .error(err => done(err));
-      });
-    } else {
-      done(validation.errors);
-    }
+    db.connect().then((dbConn) => {
+      r.table(this.tableName)
+        .get(taskId)
+        .update(task, config.model.update)
+        .run(dbConn)
+        .then(result => {
+          const { new_val } = result.changes[0] || {};
+          done(null, new_val);
+        })
+        .error(err => done(err));
+    });
   }
 
   delete(taskId, done) {
